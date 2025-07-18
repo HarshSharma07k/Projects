7abbcc9b9e7372b63475a9522c92c7cc0994a1c7
Harsh Sharma <Harshsharma29073@gmail.com>
2025-07-18 17:16:31 +0530

    build the sidebar component and sidebar items

--- fluently/app/(main)/layout.tsx
+++ fluently/app/(main)/layout.tsx
@@ -1,20 +1,23 @@
 import { MobileHeader } from "@/components/mobileHeader";
 import { Sidebar } from "@/components/sidebar";
+import { ClerkProvider } from "@clerk/nextjs";
 
 type Props = {
     children: React.ReactNode;
 };
 
-const MainLayout = ({children}: Props) => {
+const MainLayout = ({ children }: Props) => {
     return (
         <>
-            <MobileHeader />
-            <Sidebar className="hidden lg:flex"/>
-            <main className="lg:pl-[256px] h-full pt-[50px] lg:pt-0">
-                <div className="bg-indigo-500 h-full">
-                    {children}
-                </div>
-            </main>
+            <ClerkProvider>
+                <MobileHeader />
+                <Sidebar className="hidden lg:flex"/>
+                <main className="lg:pl-[256px] h-full pt-[50px] lg:pt-0">
+                    <div className="bg-indigo-500 h-full">
+                        {children}
+                    </div>
+                </main>
+            </ClerkProvider>
         </>
     );
 };
--- fluently/app/(marketing)/layout.tsx
+++ fluently/app/(marketing)/layout.tsx
@@ -4,7 +4,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 type Props = {
     children: React.ReactNode;
 };
-const MarketingLayout = ({children}: Props) => {
+const MarketingLayout = ({ children }: Props) => {
     return (
         <ClerkProvider>
             <div className="min-h-screen flex flex-col">
